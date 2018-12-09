36e075eb9a45472ac6a2cc708ca903274abbdea8
jakubwojcik <jakubwojcik.post@gmail.com>
2018-12-09 18:53:32 +0100

    Adding tests, creating algorithm class

---
+++ src/algorithm/GeneticAlgorithm.java
@@ -0,0 +1,13 @@
+package algorithm;
+
+public class GeneticAlgorithm {
+
+    public static void main(String[] args) {
+        System.out.println("Starting Genetic Algorithm");
+    }
+
+    public void createPopulation() {
+
+    }
+
+}
