8a61dde25dab671d8cbb8c00d02640a6e5dc97fc
jakubwojcik <jakubwojcik.post@gmail.com>
2018-12-09 21:35:31 +0100

    Defining basic operations and tests for them

---
+++ src/algorithm/AlgorithmSettings.java
@@ -0,0 +1,7 @@
+package algorithm;
+
+public class AlgorithmSettings {
+
+    public final int POPULATION_SIZE = 20;
+
+}
---
+++ src/algorithm/operators/MutationOperator.java
@@ -0,0 +1,9 @@
+package algorithm.operators;
+
+import game.Genome;
+
+public interface MutationOperator {
+
+    void mutateGenome(Genome genome);
+
+}
