1c2946ecc4a8021a3a592a10e5d286fa7640570d
Warzecha <Amen&attack666>
2018-12-16 17:36:07 +0100

    Battle class constructor and getters implementation. nextMoment method tests.

--- src/game/Battle.java
+++ src/game/Battle.java
@@ -1,4 +1,24 @@
 package game;
 
 public class Battle {
+
+    Ship ship1;
+    Ship ship2;
+    int time = 0;
+
+    public Battle(Ship ship1, Ship ship2) {
+        this.ship1 = ship1;
+        this.ship2 = ship2;
+    }
+
+    public Ship getShip1() {
+        return ship1;
+    }
+    public Ship getShip2() {
+        return ship2;
+    }
+
+    public int getTime() {
+        return time;
+    }
 }
