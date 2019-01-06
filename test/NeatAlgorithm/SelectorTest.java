09b10a7cd13b11426acebbef7ad060edad7d5901
Warzecha <damian.warzecha1@gmail.com>
2019-01-06 15:23:29 +0100

    simple Selector test

--- test/NeatAlgorithm/SelectorTest.java
+++ test/NeatAlgorithm/SelectorTest.java
@@ -1,4 +1,36 @@
 package NeatAlgorithm;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SelectorTest {
+
+    Species species;
+    Selector selector = new Selector();
+
+
+    @BeforeEach
+    void setup() {
+        species = new Species(new Genome(1, 2));
+
+        species.addGenome(new Genome(1, 2));
+        species.addGenome(new Genome(1, 2));
+        species.addGenome(new Genome(1, 2));
+        species.addGenome(new Genome(1, 2));
+        species.addGenome(new Genome(1, 2));
+        species.addGenome(new Genome(1, 2));
+        species.addGenome(new Genome(1, 2));
+    }
+
+
+    @Test
+    void applySelectionDecresesSizeOfSpecies() {
+        assertTrue(selector.applySelection(species).size() < 8);
+    }
+
+
+
+
 }
